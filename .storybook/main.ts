d3b31e19682ea0e48933579d1e33b61f0a3b294c
hd2908 <im2908@naver.com>
2024-03-14 17:26:55 +0900

    storybook 세팅

---
+++ .storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-themes"
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {
+
+    },
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
