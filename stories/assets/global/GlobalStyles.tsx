5cc172ace54cd6c68a4a68fe6b6a54676dcc4c87
hd2908 <im2908@naver.com>
2024-03-20 13:38:11 +0900

    Global font를 사용할 필요가 없다고 생각함

--- stories/assets/global/GlobalStyles.tsx
+++ stories/assets/global/GlobalStyles.tsx
@@ -1,33 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-import PretendardBold from "../fonts/woff-subset/Pretendard-Bold.subset.woff";
-import PretendardBold2 from "../fonts/woff2-subset/Pretendard-Bold.subset.woff2";
-import PretendardRegular from "../fonts/woff-subset/Pretendard-Regular.subset.woff";
-import PretendardRegular2 from "../fonts/woff2-subset/Pretendard-Regular.subset.woff2";
-import PretendardLight from "../fonts/woff-subset/Pretendard-Light.subset.woff";
-import PretendardLight2 from "../fonts/woff2-subset/Pretendard-Light.subset.woff2";
-
 const GlobalStyles = createGlobalStyle`
-  @font-face {
-    font-family: 'Pretendard';
-    font-weight: 700;
-    font-display: swap;
-    src: url(${PretendardBold2}) format('woff2'), url(${PretendardBold}) format('woff');
-  }
-
-  @font-face {
-    font-family: 'Pretendard';
-    font-weight: 400;
-    font-display: swap;
-    src: url(${PretendardRegular2}) format('woff2'), url(${PretendardRegular}) format('woff');
-  }
-
-  @font-face {
-    font-family: 'Pretendard';
-    font-weight: 300;
-    font-display: swap;
-    src: url(${PretendardLight2}) format('woff2'), url(${PretendardLight}) format('woff');
-  }
 
 
 `;
